35a4c98692b8d20974be17c60b816e81511befef
maggiben <benjaminmaggi@gmail.com>
2016-05-31 00:15:09 -0300

    no default config

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 import Debug from 'debug';
-import config from 'config';
 import amqp from 'amqplib';
 const debug = Debug('notification:');
 
@@ -24,9 +23,10 @@ export default class Mailman {
    * Create a instance of Mailman.
    * @constructor
    */
-  constructor() {
+  constructor(config) {
+    this.config = config;
     return new Promise((resolve, reject) => {
-      amqp.connect(config.get('amqp.url')).then(connection => {
+      amqp.connect(this.config.url).then(connection => {
         debug('amqp connection ready');
         this.connection = connection;
         this.channels = {};
@@ -47,8 +47,7 @@ export default class Mailman {
    */
   onConnect(connection) {
     return new Promise((resolve, reject) => {
-      let exchanges = config.get('amqp.exchanges');
-      let promises = Object.keys(exchanges).map(this.getChannel.bind(this));
+      let promises = Object.keys(this.config.exchanges).map(this.getChannel.bind(this));
       Promise.all(promises).then(channels => {
         return resolve(channels);
       }).catch(error => {
@@ -95,13 +94,12 @@ export default class Mailman {
    * @param {string} name - exchange name.
    */
   getChannel(name) {
-    let exchanges = config.get('amqp.exchanges');
     return new Promise((resolve, reject) => {
       this.connection.createConfirmChannel()
       .then(channel => {
         debug(`${name} channel open`);
         this.channels[name] = channel;
-        let options = exchanges[name];
+        let options = this.config.exchanges[name];
         channel.assertExchange(name, 'topic', options)
         .then(() => {
           debug(`${name} exchange open`);
